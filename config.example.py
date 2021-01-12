108b37509eeb407eea31db4ac0f0be8956bb7422
Andrei Jiroh Halili <andreijiroh@madebythepins.tk>
2021-01-12 17:44:30 +0800

    Updated the example config file
    Root user config, blah blah blah.

--- config.example.py
+++ config.example.py
@@ -1,7 +1,18 @@
+# Comment out line 2 if you don't use your
+# public IP address as your API host.
 from requests import get
+
 # Basic Bot config
 TG_BOT_TOKEN = "123456:someRandomTextGoesHere"
 LOG_CHAT = 777000
+
+# Root API key is same as an Sudo API key
+# But this will not stored on your database for security reasons.
+# See the README regarding editing this variable
+EMERGENCY_API_KEY = "replaceMeWithGeneratedText"
+# Only disable root user if you have the admin key generated through API.
+# You don't need root if your elevated_users.json is there.
+DISABLE_ROOT_USER = False
 
 # Database URL required for storing API keys and permissions.
 MONGO_DB_HOST = "insertDbUrl"
@@ -15,3 +26,7 @@ API_HOST = get('https://api.ipify.org').text
 # documentation site for detials on how to obtain these.
 SPAMWATCH_API_KEY = "replaceMeWithYourAPIKeyHere"
 SPAMBLOCKERS_API_KEY = "replaceMeWithYourAPIKeyHere"
+
+# Finally, set this variable to "NOT ANYMORE"
+# so the bot + server will run.
+__IS_THIS_STILL_AN_EXAMPLE_CONFIG = "PLZ CHANGE THIS STRING OR NO U"
