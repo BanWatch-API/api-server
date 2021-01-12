7c8b297c9c404cd0645098635d75efbe9266464b
Andrei Jiroh Halili <andreijiroh@madebythepins.tk>
2021-01-12 16:29:55 +0800

    Add my first version of my shitty code.

---
+++ BWACore.py
@@ -0,0 +1,65 @@
+#Import required dependencies first
+import asyncio
+import os
+import sys
+import time
+import flask
+from telegram.ext import CommandHandler, Updater
+from requests import get, post
+from logging import basicConfig, getLogger, INFO
+import spamwatch
+
+# Checks if going to use environment mode.
+ENV = bool(os.environ.get("ENV", False))
+if ENV:
+    ipAddr = get('https://api.ipify.org').text
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+    MONGO_DB_URL = os.environ.get("MONGO_DB_URL", None)
+    API_HOST = os.environ.get("API_HOST", ipAddr)
+    LOG_CHAT = os.environ.get("LOG_CHAT", 777000)
+else:
+    import config
+    TG_BOT_TOKEN = config.TG_BOT_TOKEN
+    MONGO_DB_URL = config.MONGO_DB_URL
+    API_HOST = config.API_HOST
+    LOG_CHAT = config.LOG_CHAT
+
+# Check if Bot API token is valid.
+# If fails either the Bot Token is blank or invalid, it will exit with error code 1.
+TG_BOT_API = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/'
+if not TG_BOT_API:
+    log.error("Bot Token is empty, exiting...")
+    exit(1)
+else:
+    checkbot = get(TG_BOT_API + 'checkMe').json()
+    if not checkbot['ok']:
+        log.error("Bot Token is invalid or Telegram went down.")
+        exit(1)
+    else:
+        log.info(
+        f"[INFO] Logged in as @{username}, waiting for webhook requests...")
+
+updater = Updater(token=BOT_TOKEN, workers=1)
+dispatcher = updater.dispatcher
+
+def start(_bot, update):
+    """/start message for bot"""
+    message = update.effective_message
+    message.reply_text(
+        "**Welcome to BanWatch API Key Manager Bot!**\n\nManage your API keys without leaving Telegram easily.\n\nNeed help? Join @BanWatchAPISupport.",
+        parse_mode="markdown")
+
+start_handler = CommandHandler("start", start)
+help_handler = CommandHandler("help", help)
+sourceCode = CommandHandler("source", source)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(sourceCode)
+updater.start_polling()
+
+
+__name__ == "__main__":
+    # We can't use port 80 due to the root access requirement.
+    port = int(environ.get("PORT", 8080))
+    server.run(host="0.0.0.0", port=port)
